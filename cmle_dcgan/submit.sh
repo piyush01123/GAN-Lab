2c905a08a6619c700f2b1488bcaec51f78b8292e
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-02-07 20:30:05 +0530

    DCGAN on Cloud ML Engine

---
+++ cmle_dcgan/submit.sh
@@ -0,0 +1,17 @@
+
+BUCKET_NAME=directed-radius-213506.appspot.com
+DATE=`date '+%Y%m%d_%H%M%S'`
+export JOB_NAME=mnist_gan_$DATE
+export JOB_DIR=gs://$BUCKET_NAME/jobs/$JOB_NAME
+export SAVE_DIR=gs://$BUCKET_NAME/save
+export REGION=us-central1
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+    --stream-logs \
+    --runtime-version 1.12 \
+    --job-dir=$JOB_DIR \
+    --python-version 3.5 \
+    --package-path=trainer \
+    --module-name trainer.task \
+    --region $REGION -- \
+    --save-dir $SAVE_DIR
---
+++ cmle_dcgan/trainer/task.py
@@ -0,0 +1,59 @@
+import argparse
+import logging
+import os
+
+from . import model
+from . import utils
+
+import tensorflow as tf
+from tensorflow.contrib.training.python.training import hparam
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num-steps',
+                        type=int,
+                        default=4000,
+                        required=False,
+                        help='Number of steps or epochs to train'
+                        )
+    parser.add_argument('--save-dir',
+                        type=str,
+                        required=True,
+                        help='Generated image save directory local or GCS'
+                        )
+    parser.add_argument('--save-interval',
+                        type=int,
+                        default=100,
+                        help='Generated image save interval'
+                        )
+    parser.add_argument('--batch-size',
+                        type=int,
+                        default=32,
+                        help='Generated image save directory local or GCS'
+                        )
+    parser.add_argument('--verbosity',
+                        choices=['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+                        default='ERROR'
+                        )
+    parser.add_argument('--job-dir',
+                        required=False,
+                        help='Some problem with submit bash command forces me to write this'
+                        )
+    return parser.parse_args()
+
+
+def train_and_evaluate(hparams):
+    dcgan = model.DCGAN(num_steps=hparams.num_steps,
+                        save_dir=hparams.save_dir,
+                        save_interval=hparams.save_interval,
+                        batch_size=hparams.batch_size
+                        )
+    dcgan.train()
+
+
+if __name__ == '__main__':
+  args = get_args()
+  tf.logging.set_verbosity(args.verbosity)
+  hparams = hparam.HParams(**args.__dict__)
+  train_and_evaluate(hparams)
