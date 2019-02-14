61b69937e574fe6c7ff5d780582eec6c9197f526
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-02-14 15:01:21 +0530

    model files git ignored

--- dcgan/dcgan.py
+++ dcgan/dcgan.py
@@ -87,28 +87,28 @@ class DCGAN:
 
 
     def train(self):
-        # valids = np.ones((self.batch_size, 1))
-        # fakes = np.zeros((self.batch_size, 1))
-        # for step in range(self.num_steps):
-        #     images_real = self.get_real_images_batch()
-        #     d_loss_real = self.D.train_on_batch(images_real, valids)
-        #     noise = np.random.normal(0, 1, size=[self.batch_size, self.latent_dim])
-        #     images_fake = self.G.predict(noise)
-        #     d_loss_fake = self.D.train_on_batch(images_fake, fakes)
-        #     d_loss, d_acc = 0.5*np.add(d_loss_real, d_loss_fake)
-        #
-        #     g_loss = self.AM.train_on_batch(noise, valids)
-        #
-        #     loss_msg = {'Step': step, 'D_loss': d_loss, 'D_acc': 100*d_acc, 'G_loss': g_loss}
-        #     print(loss_msg)
-        #
-        #     if step%self.save_interval==0:
-        #         self.plot_images(step)
-        #
-        # self.plot_images(step)
-        # self.D.save('discriminator.h5')
-        # self.G.save('generator.h5')
-        # self.AM.save('adversarial.h5')
+        valids = np.ones((self.batch_size, 1))
+        fakes = np.zeros((self.batch_size, 1))
+        for step in range(self.num_steps):
+            images_real = self.get_real_images_batch()
+            d_loss_real = self.D.train_on_batch(images_real, valids)
+            noise = np.random.normal(0, 1, size=[self.batch_size, self.latent_dim])
+            images_fake = self.G.predict(noise)
+            d_loss_fake = self.D.train_on_batch(images_fake, fakes)
+            d_loss, d_acc = 0.5*np.add(d_loss_real, d_loss_fake)
+
+            g_loss = self.AM.train_on_batch(noise, valids)
+
+            loss_msg = {'Step': step, 'D_loss': d_loss, 'D_acc': 100*d_acc, 'G_loss': g_loss}
+            print(loss_msg)
+
+            if step%self.save_interval==0:
+                self.plot_images(step)
+
+        self.plot_images(step)
+        self.D.save('discriminator.h5')
+        self.G.save('generator.h5')
+        self.AM.save('adversarial.h5')
         self.create_progression_gif()
 
     def plot_images(self, step, num_images = 16):
