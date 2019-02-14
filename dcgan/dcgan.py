7afc7c9c1aa807d2710e16465cb3bc7e29b6ad96
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-02-14 14:58:20 +0530

    created gif for digit generation model

--- dcgan/dcgan.py
+++ dcgan/dcgan.py
@@ -6,8 +6,8 @@ from tensorflow.keras.datasets import mnist
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
-tf.logging.set_verbosity(tf.logging.ERROR)
+import imageio
+import glob
 
 class DCGAN:
     def __init__(self):
@@ -87,28 +87,29 @@ class DCGAN:
 
 
     def train(self):
-        valids = np.ones((self.batch_size, 1))
-        fakes = np.zeros((self.batch_size, 1))
-        for step in range(self.num_steps):
-            images_real = self.get_real_images_batch()
-            d_loss_real = self.D.train_on_batch(images_real, valids)
-            noise = np.random.normal(0, 1, size=[self.batch_size, self.latent_dim])
-            images_fake = self.G.predict(noise)
-            d_loss_fake = self.D.train_on_batch(images_fake, fakes)
-            d_loss, d_acc = 0.5*np.add(d_loss_real, d_loss_fake)
-
-            g_loss = self.AM.train_on_batch(noise, valids)
-
-            loss_msg = {'Step': step, 'D_loss': d_loss, 'D_acc': 100*d_acc, 'G_loss': g_loss}
-            print(loss_msg)
-
-            if step%self.save_interval==0:
-                self.plot_images(step)
-
-        self.plot_images('final')
-        self.D.save('discriminator.h5')
-        self.G.save('generator.h5')
-        self.AM.save('adversarial.h5')
+        # valids = np.ones((self.batch_size, 1))
+        # fakes = np.zeros((self.batch_size, 1))
+        # for step in range(self.num_steps):
+        #     images_real = self.get_real_images_batch()
+        #     d_loss_real = self.D.train_on_batch(images_real, valids)
+        #     noise = np.random.normal(0, 1, size=[self.batch_size, self.latent_dim])
+        #     images_fake = self.G.predict(noise)
+        #     d_loss_fake = self.D.train_on_batch(images_fake, fakes)
+        #     d_loss, d_acc = 0.5*np.add(d_loss_real, d_loss_fake)
+        #
+        #     g_loss = self.AM.train_on_batch(noise, valids)
+        #
+        #     loss_msg = {'Step': step, 'D_loss': d_loss, 'D_acc': 100*d_acc, 'G_loss': g_loss}
+        #     print(loss_msg)
+        #
+        #     if step%self.save_interval==0:
+        #         self.plot_images(step)
+        #
+        # self.plot_images(step)
+        # self.D.save('discriminator.h5')
+        # self.G.save('generator.h5')
+        # self.AM.save('adversarial.h5')
+        self.create_progression_gif()
 
     def plot_images(self, step, num_images = 16):
         noise = np.random.normal(0, 1, size=[num_images, self.latent_dim])
@@ -121,6 +122,13 @@ class DCGAN:
             axes[r, c].axis('off')
         fig.savefig(self.save_dir+'step_%s.png' %step)
 
+    def create_progression_gif(self):
+        with imageio.get_writer(self.save_dir+'progression.gif', mode='I') as writer:
+            filenames = glob.glob(self.save_dir+'step_*.png')
+            filenames = sorted(filenames, key = lambda k: int(k[11:-4]))
+            for filename in filenames:
+                image = imageio.imread(filename)
+                writer.append_data(image)
 
     def preprocess_data(self):
         # Scale in -1 to 1
@@ -132,7 +140,7 @@ class DCGAN:
         return self.data[idx]
 
 
-
 if __name__=='__main__':
+    tf.logging.set_verbosity(tf.logging.ERROR)
     dcgan = DCGAN()
     dcgan.train()
